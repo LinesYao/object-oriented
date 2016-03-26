45b529b1e87d8557ca018df152300ba4ffb6b2c5
syayao <554173639@qq.com>
2016-03-26 19:45:22 +0800

    Update Scan.h

--- Calculator/Scan.h
+++ Calculator/Scan.h
@@ -1,3 +1,10 @@
+/****************************************************************************
+    Author:是光不是yao  
+    Function List:
+	1. toStringQueue 接收一个四则运算表达式，将表达式对应拆分存入队列 
+	2. getQue  	获取表达式处理后的队列 	
+****************************************************************************/
+
 #include<iostream>
 #include<string>
 #include<queue>
@@ -7,20 +14,17 @@ class Scan
 {
 	public:
 		
-		/*
-			接收一个四则运算表达式，将表达式对应拆分存入队列 
-		*/ 
-		void ToStringQueue(string input);  
+		bool isTrue = true;   //判断输入是否正确（目前实现判断输入的数字是否超过十位 超过则为 false） 
+	
+	
+		void toStringQueue(string input);  
 		
 		
-		/*
-			获取表达式处理后的队列 
-		*/ 
 		queue<string> getQue();  
 	
 		
 	private:
-		bool isTrue = true;   //判断输入是否正确（目前实现判断输入的数字是否超过十位 超过则为 false） 
+		
 		queue<string> inputQue;  //存放表达式处理后的队列 
 	
 		
